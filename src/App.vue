fb4785965613de0015c8b9dc3faa0c33ba834866
Dirk Porsche <dirkporsche78@googlemail.com>
2021-02-16 21:57:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen w-screen flex flex-col pt-12">
+    <TopBar />
+    <!-- [ HERE COMES YOUR CONTENT ] -->
+    <transition name="fade" appear>
+      <div class="flex flex-col items-center justify-center h-full pb-0">
+        <img class="h-48 ml-5" src="/logo.png" draggable="false" />
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import TopBar from '/~/components/layout/TopBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TopBar,
+  },
+}
+</script>
